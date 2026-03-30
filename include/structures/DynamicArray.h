29087a32ae75dccbf6c6a502e73d772f7d5c6cb0
Mykhailo Tsonyev <>
2026-03-31 01:32:36 +0200

    Add DynamicArray example main

---
+++ include/structures/DynamicArray.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <iostream>
+#include <stdexcept>
+
+template <typename T>
+class DynamicArray {
+private:
+    T* data;
+    int size;
+
+public:
+    // Konstruktor
+    DynamicArray(int n) {
+        if (n <= 0) throw std::invalid_argument("Size must be > 0");
+        size = n;
+        data = new T[size]; // Dynamiczna alokacja
+    }
+
+    // Destruktor (zwalnianie pamięci)
+    ~DynamicArray() {
+        delete[] data;
+    }
+
+    // Zakaz kopiowania struktury (ochrona przed wyciekami i awariami)
+    DynamicArray(const DynamicArray&) = delete;
+    DynamicArray& operator=(const DynamicArray&) = delete;
+
+    // Dostęp po indeksie (jak w zwykłej tablicy)
+    T& operator[](int index) {
+        return data[index];
+    }
+
+    // Pobierz rozmiar
+    int getSize() const {
+        return size;
+    }
+
+    // Metoda do wypisywania (przydatna do debugowania)
+    void print() const {
+        for (int i = 0; i < size; i++) {
+            std::cout << data[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,31 @@
+#include <iostream>
+
+#include "structures/DynamicArray.h"
+
 int main() {
-  return 0;
+    // Primer 1: bazovy tip int
+    int sizeFromFile = 5;
+
+    DynamicArray<int> myArr(sizeFromFile);
+    myArr[0] = 76;
+    myArr[1] = -142;
+    myArr[2] = 0;
+    myArr[3] = 2138;
+    myArr[4] = -4;
+
+    std::cout << "Rozmiar tablicy: " << myArr.getSize() << "\n";
+    std::cout << "Element pod indeksem 3: " << myArr[3] << "\n";
+
+    std::cout << "Cala tablica: ";
+    myArr.print();
+
+    // Primer 2: inny typ danych
+    DynamicArray<char> charArr(3);
+    charArr[0] = 'A';
+    charArr[1] = 'B';
+    charArr[2] = 'C';
+
+    std::cout << "Pierwszy znak: " << charArr[0] << "\n";
+
+    return 0;
 }
