0d5c6fb30241354dd6a829b42b45336d7035d412
Mykhailo Tsonyev <>
2026-04-20 19:24:19 +0200

    smth

--- include/structures/DynamicArray.h
+++ include/structures/DynamicArray.h
@@ -3,26 +3,27 @@
 #include <iostream>
 #include <stdexcept>
 
+#include "IStructure.h"
+
 template <typename T>
-class DynamicArray
+class DynamicArray : public IStructure<T>
 {
 private:
     T *data;
-    int size;
+    int currentSize;
 
 public:
-    /// Konstruktor.
-    /// \param n długość (liczba elementów) tablicy dynamicznej.
+    // Konstruktor
     DynamicArray(int n)
     {
         if (n <= 0)
             throw std::invalid_argument("Size must be > 0");
-        size = n;
-        data = new T[size]; // Dynamiczna alokacja
+        currentSize = n;
+        data = new T[currentSize];
     }
 
     // Destruktor (zwalnianie pamięci)
-    ~DynamicArray()
+    ~DynamicArray() override
     {
         delete[] data;
     }
@@ -32,21 +33,64 @@ public:
     DynamicArray &operator=(const DynamicArray &) = delete;
 
     // Dostęp po indeksie (jak w zwykłej tablicy)
-    T &operator[](int index)
+    T &operator[](int index) override
     {
+        if (index < 0 || index >= currentSize)
+            throw std::out_of_range("Index out of range");
+
+        return data[index];
+    }
+
+    const T &operator[](int index) const override
+    {
+        if (index < 0 || index >= currentSize)
+            throw std::out_of_range("Index out of range");
+
         return data[index];
     }
 
     // Pobierz rozmiar
+    int size() const override
+    {
+        return currentSize;
+    }
+
     int getSize() const
     {
-        return size;
+        return currentSize;
+    }
+
+    // тут какой то буллшит
+    void pushBack(const T &value) override
+    {
+        T *newData = new T[currentSize + 1];
+
+        for (int i = 0; i < currentSize; i++)
+        {
+            newData[i] = data[i];
+        }
+
+        newData[currentSize] = value;
+
+        delete[] data;
+        data = newData;
+        currentSize++;
+    }
+
+    void swap(int i, int j) override
+    {
+        if (i < 0 || i >= currentSize || j < 0 || j >= currentSize)
+            throw std::out_of_range("Index out of range");
+
+        T temp = data[i];
+        data[i] = data[j];
+        data[j] = temp;
     }
 
     // Metoda do wypisywania (przydatna do debugowania)
     void print() const
     {
-        for (int i = 0; i < size; i++)
+        for (int i = 0; i < currentSize; i++)
         {
             std::cout << data[i] << " ";
         }
--- include/structures/IStructure.h
+++ include/structures/IStructure.h
@@ -1,5 +1,6 @@
-﻿#pragma once
+#pragma once
 
+template <typename T>
 class IStructure
 {
 public:
@@ -7,8 +8,8 @@ public:
 
     virtual int size() const = 0; // = 0 znaczy ze niema realizacji w bazowym klasie
 
-    virtual int &operator[](int index) = 0;             // dostemp przez array[i]
-    virtual const int &operator[](int index) const = 0; // dla const DynamicArray arr(5); arr[0] = 7; niemozna
-    virtual void pushBack(const int &value) = 0;
+    virtual T &operator[](int index) = 0;             // dostemp przez array[i]
+    virtual const T &operator[](int index) const = 0; // dla const DynamicArray arr(5); arr[0] = 7; niemozna
+    virtual void pushBack(const T &value) = 0;
     virtual void swap(int i, int j) = 0;
 };
